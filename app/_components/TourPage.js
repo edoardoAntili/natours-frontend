d97e92a2fd485238ed5a0a497a628fa2a8bb998c
Edoardo <edoardo@Edoardos-MacBook-Pro.local>
2026-09-25 22:40:37 +0200

    Checking for vercel error during production

--- app/_components/TourPage.js
+++ app/_components/TourPage.js
@@ -95,6 +95,7 @@ async function PersonalBookingSection({ tour, slug }) {
   if (user) {
     const jwt = (await cookies()).get("jwt")?.value;
     result = await getPersonalTourBySlug(slug, jwt);
+    console.log(result);
     if (
       result.status === "success" &&
       result.tour.availableStartDates.length === 0
